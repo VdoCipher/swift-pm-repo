e54f56ab4301b5850f385f4f94678f53cb2338a5
Vibhav S <v@vsinha.me>
2022-10-18 13:38:24 +0530

    updated to 2.1.3

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VdoFramework",
-            url: "https://vdocipher.s3.amazonaws.com/archive/VdoFramework-2.1.2.xcframework.zip",
-            checksum: "28f4b3e831f9e8e45adb403ac8e84e4dc4d9e70ad95f05bc0cb6b71a77005f22"
+            url: "https://vdocipher.s3.amazonaws.com/archive/VdoFramework-2.1.3.xcframework.zip",
+            checksum: "83aa08ba2d4b0a8acb7fe31a6d145abc23d1c9a29e4dfcd146ab87156bb4acb5"
         )
     ]
 )
