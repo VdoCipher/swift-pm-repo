a41e52417bed3477ef870b50aa8ea85c499d7663
Manish Gupta <manish.gupta@vdocipher.com>
2026-03-11 10:30:50 +0530

    added new version 2.9.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", .exact("1.34.1")),
     ],
     targets: [
         .binaryTarget(
