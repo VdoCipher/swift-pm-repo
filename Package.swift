933078042272560306bd49cd89c32eabd34412fa
Manish Gupta <manish.gupta@vdocipher.com>
2026-03-20 16:18:16 +0530

    added new version 2.9.1

--- Package.swift
+++ Package.swift
@@ -13,13 +13,12 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-protobuf.git", .exact("1.34.1")),
     ],
     targets: [
         .binaryTarget(
             name: "VdoFramework",
-            url: "https://vdocipher.s3.amazonaws.com/archive/VdoFramework-2.9.0.xcframework.zip",
-            checksum: "bba673fbc4fd7e5e7e4711732d0f157854adee08a59ea6e940671e76ef54a3c2"
+            url: "https://vdocipher.s3.amazonaws.com/archive/VdoFramework-2.9.1.xcframework.zip",
+            checksum: "372ec783d12daefd64c4d7b74f540438d0bc6ef54a51429cbab4edaf3f717add"
         )
     ]
 )
